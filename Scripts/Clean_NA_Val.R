ff147cc21e03a610958e304eeb411d080b3dfa6d
villager1 <irisdizdari@yahoo.com>
2024-11-15 19:00:10 +0100

    deleted unnecessary lines

--- Scripts/Clean_NA_Val.R
+++ Scripts/Clean_NA_Val.R
@@ -5,15 +5,12 @@ dataCleaning <- function(data_frame) {
     if (is.numeric(data_frame[[col_name]])) {
       # Extract the column
       column <- data_frame[[col_name]]
-      
       # Check if the column is entirely zeros
       if (all(column == 0, na.rm = TRUE)) {
         next # Skip this column; leave it as is
       }
-      
       # Calculate the mean, excluding 0 and NA
       column_mean <- mean(column[column != 0 & !is.na(column)], na.rm = TRUE)
-      
       # Replace 0s and NAs with the mean
       data_frame[[col_name]][column == 0 | is.na(column)] <- column_mean
     }
@@ -22,6 +19,3 @@ dataCleaning <- function(data_frame) {
   return(data_frame)
 }
 
-cleanDfLinne <- dataCleaning(roundLogs_Linne)
-cleanDfDame <- dataCleaning(roundLogs_Dame)
-is.na(cleanDfLinne)
