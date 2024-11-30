085200a457157e690aad342f28fa627c92f06e53
villager1 <irisdizdari@yahoo.com>
2024-11-30 14:05:16 +0100

    readding the NA clean function into main

---
+++ Script/cleanData.R
@@ -0,0 +1,29 @@
+dataCleaning <- function(dataFrame) {
+  # Iterate through each column of the data frame
+  for (colName in names(dataFrame)) {
+    # Check if the column is numeric
+    if (is.numeric(dataFrame[[colName]])) {
+      # Extract the column
+      column <- dataFrame[[colName]]
+      
+      # Check if the column is entirely zeros
+      if (all(column == 0, na.rm = TRUE)) {
+        next # Skip this column; leave it as is
+      }
+      
+      # Calculate the mean, excluding 0 and NA
+      columnMean <- mean(column[column != 0 & !is.na(column)], na.rm = TRUE)
+      
+      # Replace 0s and NAs with the mean
+      dataFrame[[colName]][column == 0 | is.na(column)] <- columnMean
+    }
+  }
+  # Return the cleaned data frame
+<<<<<<< Updated upstream:Script/cleanData.R
+  return(dataFrame)
+}
+
+=======
+  return(data_frame)
+}
+>>>>>>> Stashed changes:Scripts/Clean_NA_Val.R
