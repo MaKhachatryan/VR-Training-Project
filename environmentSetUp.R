a1c6795b2e4da5f99a3705056ff7052ac7ab7f11
Anh Dang <anhdang@Mac.fritz.box>
2025-01-02 15:02:13 +0100

    script change

--- environmentSetUp.R
+++ environmentSetUp.R
@@ -30,9 +30,9 @@ for (ds in dataSet) {
 }
 
 ## ----- Sourcing functions -----
-source("Script/utils/fixPMD.R")
-source("Script/utils/cleanData.R")
-source("Script/utils/pivotedData.R")
+source("Script/fixPMD.R")
+source("Script/cleanData.R")
+source("Script/pivotedData.R")
 functionFolder <- "Script"
 scriptFiles <- list.files(path = functionFolder, pattern = "\\.R$", recursive = TRUE, full.names = TRUE)
 lapply(scriptFiles, source)
